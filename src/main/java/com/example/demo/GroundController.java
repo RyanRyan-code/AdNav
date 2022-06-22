02a0322fad98f630d0c59edca2f54f0f3d85b02b
Ryan Wang <ryan@192-168-1-2.tpgi.com.au>
2022-06-22 15:50:20 +1000

    raw websocket config

---
+++ src/main/java/com/example/demo/GroundController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
+
+@EnableScheduling
+@Controller
+public class GroundController {
+
+
+
+
+}
